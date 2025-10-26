0b92be36069a1c6cb0603471cf6934bf33853a77
Tawhidul Islam <138350536+tawhid3482@users.noreply.github.com>
2025-10-26 20:35:22 +0600

    refactor

--- src/modules/testimonials/testimonials.validation.ts
+++ src/modules/testimonials/testimonials.validation.ts
@@ -1,13 +1,7 @@
 import { z } from "zod";
 
-// --- If you already have a Status enum in Prisma, include it like this ---
-export const StatusEnum = z.enum(["PENDING", "CONFIRMED", "REJECTED"]);
-
 export const testimonialSchema = z.object({
-  content: z
-    .string(),
-  name: z.string().min(2, "Name must be at least 2 characters long"),
-  Image: z.string().url("Image must be a valid URL"),
-  status: StatusEnum.default("CONFIRMED"),
-  createdAt: z.date().optional(),
+  content: z.string(),
+  name: z.string(),
+  Image: z.string()
 });
